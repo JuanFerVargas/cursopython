a624938178524767449ecacff7d7fddc29b0ba83
juanfernandovargas <juanfernandovargas@gmail.com>
2025-06-18 03:14:24 +0000

    Exceptions

---
+++ Curso_Python/12_Archivos/01_Manejo_Archivos.py
@@ -0,0 +1,11 @@
+try:
+    archivo=open('\\home\\user\\cursopython\\Curso_Python\\12_Archivos\\prueba.txt','w',encoding='utf8')      #
+    
+    archivo.write('Esto es una prueba\n')
+    archivo.write('Está es una prueba\n')
+
+except Exception as e:
+    print(e)
+finally:
+    archivo.close()
+    print('Se ha finalizado la prueba')
